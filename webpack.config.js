139d34734c4adeb41c345835b23d4cc75d147cd9
My Kim Bui <mykimbuibui@gmail.com>
2017-05-12 15:40:25 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: './public/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['','.js','.jsx']
+  }
+};
